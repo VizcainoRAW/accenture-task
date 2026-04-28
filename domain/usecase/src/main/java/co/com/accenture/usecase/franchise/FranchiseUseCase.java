37be3343b65c61751293f5c7723ff9a47b50aaf8
Ivan Vizcaino <ivanandres1305@gmail.com>
2026-04-28 16:23:31 -0500

    Add FranchiseUseCase with CRUD operations

---
+++ domain/usecase/src/main/java/co/com/accenture/usecase/franchise/FranchiseUseCase.java
@@ -0,0 +1,29 @@
+package co.com.accenture.usecase.franchise;
+
+import co.com.accenture.model.franchise.Franchise;
+import co.com.accenture.model.franchise.gateways.FranchiseRepository;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+public class FranchiseUseCase {
+
+    private final FranchiseRepository franchiseRepository;
+
+    public Mono<Franchise> findById(String id) {
+        return franchiseRepository.findById(id);
+    }
+    public Mono<Franchise> save(Franchise franchise) {
+        return franchiseRepository.save(franchise);
+    }
+
+    public Mono<Void> deleteById(String id) {
+        return franchiseRepository.deleteById(id);
+    }
+
+    public Mono<Boolean> existsById(String id) {
+        return franchiseRepository.existsById(id);
+    }
+
+
+}
