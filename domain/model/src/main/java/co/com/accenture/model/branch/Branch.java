56ca0b50d270918befa8ed0cf29a460cc8b71976
Ivan Vizcaino <ivanandres1305@gmail.com>
2026-04-28 15:29:53 -0500

    Add Branch model and repository interface

---
+++ domain/model/src/main/java/co/com/accenture/model/branch/Branch.java
@@ -0,0 +1,19 @@
+package co.com.accenture.model.branch;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Branch {
+
+    private String id;
+
+    private String name;
+
+}
---
+++ domain/model/src/main/java/co/com/accenture/model/branch/gateways/BranchRepository.java
@@ -0,0 +1,18 @@
+package co.com.accenture.model.branch.gateways;
+
+import co.com.accenture.model.branch.Branch;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface BranchRepository {
+
+    Flux<Branch> findAll();
+
+    Mono<Branch> findById(String id);
+
+    Mono<Branch> save(Branch f);
+
+    Mono<Void> deleteById(String id);
+
+    Mono<Boolean> existsById(String id);
+}
