f7295cf1cfeb0be933437b25ceb5da1ade6511ef
Ivan Vizcaino <ivanandres1305@gmail.com>
2026-04-29 08:26:13 -0500

    Add FranchiseReactiveRepository interface with Reactive CRUD and query support

---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/accenture/r2dbc/franchise/FranchiseReactiveRepository.java
@@ -0,0 +1,12 @@
+package co.com.accenture.r2dbc.franchise;
+
+import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface FranchiseReactiveRepository extends
+        ReactiveCrudRepository<FranchiseData, String>,
+        ReactiveQueryByExampleExecutor<FranchiseData> {
+
+    Mono<FranchiseData> findByName(String name);
+}
