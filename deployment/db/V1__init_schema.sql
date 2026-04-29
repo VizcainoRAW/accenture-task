22e99fcb023f4c9941dab62259c933fad5a97c2e
Ivan Vizcaino <ivanandres1305@gmail.com>
2026-04-29 14:29:58 -0500

    Update init schema: switch to `UUID` for primary keys, add `pgcrypto` extension, and enforce `ON DELETE CASCADE` constraints

--- deployment/db/V1__init_schema.sql
+++ deployment/db/V1__init_schema.sql
@@ -1,17 +1,19 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE IF NOT EXISTS franchises (
-                                          id   VARCHAR(36)  PRIMARY KEY,
+                                          id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255) NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS branches (
-                                        id           VARCHAR(36)  PRIMARY KEY,
-    name         VARCHAR(255) NOT NULL,
-    franchise_id VARCHAR(36)  NOT NULL REFERENCES franchises(id)
+                                        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    franchise_id UUID NOT NULL REFERENCES franchises(id) ON DELETE CASCADE
     );
 
 CREATE TABLE IF NOT EXISTS products (
-                                        id        VARCHAR(36)  PRIMARY KEY,
-    name      VARCHAR(255) NOT NULL,
-    stock     INTEGER      NOT NULL DEFAULT 0,
-    branch_id VARCHAR(36)  NOT NULL REFERENCES branches(id)
+                                        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    stock INTEGER NOT NULL DEFAULT 0,
+    branch_id UUID NOT NULL REFERENCES branches(id) ON DELETE CASCADE
     );
